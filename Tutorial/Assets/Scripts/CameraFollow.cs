a74af7f5042d4a35f0ae25bbdb71a1fc4401d25f
yuwenbai@163.com <yuwenbai@163.com>
2017-02-26 18:22:21 +0800

    delete objs

---
+++ Tutorial/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject Targetplayer;
+    public int nDistance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 targetPos = Targetplayer.transform.position + new Vector3(0, 3, -6);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPos, nDistance * Time.deltaTime);
+
+    }
+}
---
+++ Tutorial/Assets/Scripts/ShooterMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterMove : MonoBehaviour {
+    public float speed = 5.0f;
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float w = Input.GetAxis("Vertical");
+        //gameObject.transform.Translate(new Vector3(h, 0, w)*Time.deltaTime);
+        gameObject.GetComponent<Rigidbody>().MovePosition(gameObject.transform.position + new Vector3(h, 0, w) * Time.deltaTime);
+        if(w!=0 || h != 0)
+        {
+            anim.SetBool("Move", true);
+            bool bMove = anim.GetBool("Move");
+            //Debug.Log("move state is " + bMove);
+        }else
+        {
+            anim.SetBool("Move", false);
+            bool bMove = anim.GetBool("Move");
+            //Debug.Log("move state is " + bMove);
+        }
+    }
+}
